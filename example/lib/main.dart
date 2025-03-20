9f6d6ab7ad3121606d9e1622c27f0758045cddb1
treeplate <tree.plate.moo@gmail.com>
2025-03-19 17:50:27 -0700

    improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,12 @@
+import 'dart:ffi';
 import 'dart:math';
 
+import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
 import 'package:ortho3d/ortho3d.dart';
+import 'package:win32/win32.dart';
 
 void main() {
   runApp(const ExampleApp());
@@ -16,60 +18,38 @@ class ExampleApp extends StatefulWidget {
   State<ExampleApp> createState() => _ExampleAppState();
 }
 
+List<Triangle> square(Vector3 a, Vector3 b, Vector3 c, Vector3 d, Color color) {
+  return [(a: a, b: b, c: d, color: color), (a: c, b: b, c: d, color: color)];
+}
+
 class _ExampleAppState extends State<ExampleApp>
     with SingleTickerProviderStateMixin {
   Vector3 angle = Vector3(0, 0, 0);
   Vector3 position = Vector3(0, 0, 0);
-  static const double triangleDistance = 2*sqrt2;
+  static const double triangleDistance = 2;
+  static final Vector3 p111 =
+      Vector3(triangleDistance, triangleDistance, triangleDistance);
+  static final Vector3 p110 =
+      Vector3(triangleDistance, triangleDistance, -triangleDistance);
+  static final Vector3 p101 =
+      Vector3(triangleDistance, -triangleDistance, triangleDistance);
+  static final Vector3 p100 =
+      Vector3(triangleDistance, -triangleDistance, -triangleDistance);
+  static final Vector3 p011 =
+      Vector3(-triangleDistance, triangleDistance, triangleDistance);
+  static final Vector3 p010 =
+      Vector3(-triangleDistance, triangleDistance, -triangleDistance);
+  static final Vector3 p001 =
+      Vector3(-triangleDistance, -triangleDistance, triangleDistance);
+  static final Vector3 p000 =
+      Vector3(-triangleDistance, -triangleDistance, -triangleDistance);
   List<Triangle> triangles = [
-    (
-      a: Vector3(triangleDistance, 0, 0),
-      b: Vector3(0, triangleDistance, 0),
-      c: Vector3(0, 0, triangleDistance),
-      color: Color(0xFFa0a0a0)
-    ),
-    (
-      a: Vector3(triangleDistance, 0, 0),
-      b: Vector3(0, triangleDistance, 0),
-      c: Vector3(0, 0, -3),
-      color: Color(0xFF0000FF)
-    ),
-    (
-      a: Vector3(triangleDistance, 0, 0),
-      b: Vector3(0, -3, 0),
-      c: Vector3(0, 0, triangleDistance),
-      color: Color(0xFF00FF00)
-    ),
-    (
-      a: Vector3(triangleDistance, 0, 0),
-      b: Vector3(0, -3, 0),
-      c: Vector3(0, 0, -3),
-      color: Color(0xFF00FFFF)
-    ),
-    (
-      a: Vector3(-triangleDistance, 0, 0),
-      b: Vector3(0, triangleDistance, 0),
-      c: Vector3(0, 0, triangleDistance),
-      color: Color(0xFFFF0000)
-    ),
-    (
-      a: Vector3(-triangleDistance, 0, 0),
-      b: Vector3(0, triangleDistance, 0),
-      c: Vector3(0, 0, -3),
-      color: Color(0xFFFF00FF)
-    ),
-    (
-      a: Vector3(-triangleDistance, 0, 0),
-      b: Vector3(0, -3, 0),
-      c: Vector3(0, 0, triangleDistance),
-      color: Color(0xFFFFFF00)
-    ),
-    (
-      a: Vector3(-triangleDistance, 0, 0),
-      b: Vector3(0, -3, 0),
-      c: Vector3(0, 0, -3),
-      color: Color(0xFFFFFFFF)
-    ),
+    ...square(p000, p010, p011, p001, Color(0xffff0000)), // 00
+    ...square(p000, p010, p110, p100, Color(0xff0000ff)), // 20
+    ...square(p000, p100, p101, p001, Color(0xff00ff00)), // 10
+    ...square(p111, p011, p010, p110, Color(0xffff00ff)), // 11
+    ...square(p111, p101, p001, p011, Color(0xffffff00)), // 21
+    ...square(p111, p101, p100, p110, Color(0xff00ffff)), // 01
   ];
 
   late final Ticker ticker;
@@ -79,15 +59,23 @@ class _ExampleAppState extends State<ExampleApp>
     super.initState();
   }
 
-  bool rotating = false;
-
   void tick(Duration d) {
-    setState(() {
-      if (rotating) {
-        angle.y += .01;
-        angle.x += .01;
-      }
-    });
+    int window = GetForegroundWindow();
+    Pointer<RECT> rectPtr = malloc.allocate<RECT>(sizeOf<RECT>());
+    if (GetWindowRect(window, rectPtr) == 0) {
+      print('error: ${GetLastError()}');
+    } else {
+      Pointer<POINT> pointPtr = malloc.allocate<POINT>(sizeOf<POINT>());
+      GetCursorPos(pointPtr);
+
+      int newX = rectPtr.ref.left + rectPtr.ref.right ~/ 2;
+      int newY = rectPtr.ref.top + rectPtr.ref.bottom ~/ 2;
+      angle.y -= (newX - pointPtr.ref.x) / 100;
+      angle.x =
+          clampDouble(angle.x + (newY - pointPtr.ref.y) / 100, -pi / 2, pi / 2);
+      SetCursorPos(newX, newY);
+    }
+    malloc.free(rectPtr);
   }
 
   @override
@@ -96,44 +84,12 @@ class _ExampleAppState extends State<ExampleApp>
     super.dispose();
   }
 
-  FocusNode focusNode = FocusNode();
-
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onHover: (details) {
-        angle.y += details.delta.dx/100;
-        angle.x = clampDouble(angle.x-details.delta.dy/100, -pi/2, pi/2);
-      },
-      child: KeyboardListener(
-        focusNode: focusNode,
-        onKeyEvent: (key) {
-          switch(key.logicalKey) {
-            case LogicalKeyboardKey.arrowLeft:
-              setState(() {
-                angle.y-=.05;
-              });
-            case LogicalKeyboardKey.arrowRight:
-              setState(() {
-                angle.y+=.05;
-              });
-            case LogicalKeyboardKey.arrowUp:
-              setState(() {
-                angle.x+=.05;
-              });
-            case LogicalKeyboardKey.arrowDown:
-              setState(() {
-                angle.x-=.05;
-              });
-          }
-        },
-        autofocus: true,
-        child: TrianglesRenderer3D(
-          triangles: triangles,
-          angle: angle,
-          position: position,
-        ),
-      ),
+    return TrianglesRenderer3D(
+      triangles: triangles,
+      angle: angle,
+      position: position,
     );
   }
 }
